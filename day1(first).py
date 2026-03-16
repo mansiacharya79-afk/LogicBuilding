a90498a0e92591a698d963f6a3957a96ba779148
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-16 21:20:14 +0530

    day 1

--- day1(first).py
+++ day1(first).py
@@ -1,7 +1,7 @@
 '''Program to Print Integer Numbers Entered by the User:
-Write a program where the user is asked to enter an integer number, and the program prints that number back to them.For example:'''
+Write a program where the user is asked to enter an integer number, and the program prints that number back to them.'''
 
 
 
-num = int(input("Enter an integer: "))
+num = int(input("Enter an integer number: "))
 print("You entered:", num)
