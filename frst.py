322f5ceb641b6d29a6a38f67b24d8e3c8ced58f1
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-16 21:03:40 +0530

    Day 1

---
+++ frst.py
@@ -0,0 +1,7 @@
+'''Program to Print Integer Numbers Entered by the User:
+Write a program where the user is asked to enter an integer number, and the program prints that number back to them.For example:'''
+
+
+
+num = int(input("Enter an integer: "))
+print("You entered:", num)
