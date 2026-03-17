762cce9fa57b91f12d58be39f35edd3d975b6b13
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-17 20:45:28 +0530

    day2

---
+++ day2(scnd).py
@@ -0,0 +1,13 @@
+'''Program to Check Whether the Number is Divisible by 5:
+Write a program that determines if a number entered by the user is divisible by 5. '''
+
+
+num=int(input("Enter a number: "))
+if num%5==0:
+    print(num,"is divisible by 5")
+else:
+    print(num,"is not divisible by 5")
+
+'''output:
+Enter a number: 15
+15 is divisible by 5'''
