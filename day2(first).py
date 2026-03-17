4ed6f2f26df34b17c691b5fc1f17b590235b5e7a
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-17 20:38:04 +0530

    Day2

---
+++ day2(first).py
@@ -0,0 +1,18 @@
+'''Program to Check Whether the Number is Odd or Even:
+Write a program that checks whether a number entered by the user is odd or even.'''
+
+
+num=int(input("Enter a number: "))
+if num%2==0:
+    print(f"The given number {num} is even")
+else:
+    print(f"The given number {num} is odd")
+
+
+'''output:
+Enter a number: 15
+The given number 15 is odd
+
+output:
+Enter a number: 20
+The given number 20 is even'''
