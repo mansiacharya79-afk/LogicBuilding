c53e50d2d63ff136a60b8c1757bb8430dda4f035
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-18 19:07:23 +0530

    day 3

---
+++ day3(first).py
@@ -0,0 +1,16 @@
+''''Program to Calculate the Square and Cube of a Number:
+Write a program where the user enters a number, and the program calculates its square and cube, and then prints the results.'''
+
+
+
+
+num=int(input("Enter a number: "))
+print(f"The square of the number {num} is: {num**2}")
+print(f"the cube of the number {num} is :{num**3}")
+
+
+
+'''Output:
+Enter a number: 5
+The square of the number 5 is: 25
+the cube of the number 5 is :125'''
