921f0c9e599c40443228796e5f55bc1989050c1d
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-19 19:54:18 +0530

    day4

---
+++ day4(frst).py
@@ -0,0 +1,20 @@
+'''Print the Multiplication Table of a Given Number:
+Write a program where the user enters a number, and the program prints its multiplication table. For example:
+'''
+
+num = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
+
+
+
+
+'''Output:
+Enter a number: 4
+4 x 1 = 4
+4 x 2 = 8
+.
+.
+.
+
+4 x 10 = 40 '''
