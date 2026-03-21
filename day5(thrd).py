19cc3c74a23a8f8f3b9bbacd440f932b68f5c059
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-21 19:44:01 +0530

    updated

--- day5(thrd).py
+++ day5(thrd).py
@@ -1,3 +1,12 @@
+'''Write a Program to Find the ASCII Value of a Character:
+Write a program that takes a character as input and displays its ASCII value.'''
+
+
 char=input("Enter the character:")
 result=ord(char)
 print(f"The ASCII value of {char} is:",result)
+
+
+'''output:
+Enter the character: A
+The ASCII value of A is: 65'''
