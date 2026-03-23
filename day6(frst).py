c286b9bbbca979c9e674b8d9d72c59df7e508b21
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-23 19:53:00 +0530

    day6

---
+++ day6(frst).py
@@ -0,0 +1,23 @@
+'''Pattern 1: Sqaure
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+
+
+
+for i in range(5):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+
+
+
+'''output:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *'''
