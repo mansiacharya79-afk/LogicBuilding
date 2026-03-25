b3989a94ef000350e1d07890a96f9de2a5de0e03
Mansi Acharya <mansiacharya79@gmail.com>
2026-03-25 19:11:17 +0530

    day8

--- day7(thrd).py
+++ day7(thrd).py
@@ -1,6 +1,16 @@
+'''Inverted traingle
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+
+'''
+
+
+
 n = 5
 space = 0
-
 for i in range(n, 0, -1):
     print(" " * space + "* " * (2*i - 1))
     space += 2
---
+++ day8(thrd).py
@@ -0,0 +1,14 @@
+'''Pattern 9
+1 2 3 4 5  
+1 2 3 4  
+1 2 3  
+1 2  
+1  
+'''
+
+
+n = 6
+for i in range(n, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
